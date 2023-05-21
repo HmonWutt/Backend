a9f2153b6eedb25da2bedaa3fb3d09321877b4c3
hmon <wu2504hm-s@student.lu.se>
2023-05-21 23:02:43 +0200

    Tasklist table working

--- client/src/index.js
+++ client/src/index.js
@@ -1,7 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
-import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import "bootstrap/dist/css/bootstrap.min.css";
 import Tasklist from "./tasklist";
@@ -11,7 +10,6 @@ const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <Tasklist />
-   
   </React.StrictMode>
 );
 

76896ee1c948fc465c2de1661ceadbaa99294f16
hmon <wu2504hm-s@student.lu.se>
2023-07-05 16:36:19 +0200

    add password file to gitignore

---
+++ google-credentials.js
@@ -0,0 +1,7 @@
+module.exports = {
+  CLIENT_ID:
+    "539052762848-4ehetuj80a9aob5qvk8g67jouv4o7s3v.apps.googleusercontent.com",
+  CLIENT_SECRET: "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",
+  REFRESH_TOKEN:
+    "1//04wFaqOoY4zz2CgYIARAAGAQSNwF-L9IrrfeCFnf-2012_hu5hKd_S1e2TLZKJRZur9ZGXbmTyJb-P2ylfoT5h-0wWrCKrvJUsuM",
+};

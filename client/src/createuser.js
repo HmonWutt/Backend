c84d806cb4d6b4dec5d9ee0375731d31f7577bba
hmon <wu2504hm-s@student.lu.se>
2023-08-10 19:09:27 +0200

    createuser works

--- client/src/createuser.js
+++ client/src/createuser.js
@@ -5,41 +5,109 @@ function Loginapp() {
   const [username, setUsername] = useState("");
   const [passwordError, setpasswordError] = useState("");
   const [usernameError, setusernameError] = useState("");
-  const [forminvalid, setForminvalid] = useState(false);
-  const [formsubmitted, setFormsubmitted] = useState(false);
+  const [userexists, setUserexists] = useState(false);
+  const [usernotexists, setUsernotexists] = useState(false);
+  let res;
 
-  const handleValidation = (event) => {
-    let formIsValid = true;
+  async function saveuser(username, password) {
+    console.log("saveuser request sent");
+    try {
+      const body = { username, password };
+      const response = await fetch("http://192.168.0.6:3000/createuser", {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify(body),
+      });
+      res = await response.json();
+      console.log(res.message);
+      if (res.message === "Username already exists.") {
+        setUserexists(true);
+        setTimeout(() => setUserexists(false), 1000);
+      } else {
+        setUsernotexists(true);
+        setUsername("");
+        setPassword("");
+        setTimeout(() => setUsernotexists(false), 1000);
+      }
 
-    if (username.length >= 5 && !usernameError && password.length >=8 && !passwordError ) {
-      formIsValid = true;
-    } else {
-      formIsValid = false;
-      setForminvalid(true)
-      //setusernameError("User name cannot be empty");
-
+      //debugger;
+    } catch (error) {
+      console.error(error.message);
     }
+  }
 
-    return formIsValid;
+  let formIsValid = true;
+  const handleValidation = (event) => {
+    console.log("handleValidation ran");
+    Checkusername(username);
+    console.log(formIsValid);
+    Checkpassword(password);
+    console.log(formIsValid);
+    console.log("username", username, "password", password);
+    if (username === "" || password === "") {
+      formIsValid = false;
+      if (username === "" && password === "") {
+        setpasswordError("Password cannot be empty!");
+        setusernameError("User name cannot be empty");
+      } else if (username === "") {
+        setusernameError("User name cannot be empty!");
+      } else {
+        setpasswordError("Password cannot be empty");
+      }
+      setTimeout(() => {
+        setusernameError("");
+        setpasswordError("");
+      }, 2000);
+    }
   };
+
   function Checkusername(username) {
-    setForminvalid(false);
     setUsername(username);
-    if (!username.match(/^[a-zA-Z0-9()]{5,10}$/)) {
-      setusernameError(
-        "Only numbers and letters and at least 5 characters long"
-      );
+    if (
+      !username.match(/^[a-zA-Z0-9]{5,10}$/) ||
+      username.length < 5 ||
+      username.length > 10
+    ) {
+      formIsValid = false;
+
+      if (
+        (username.length < 5 || username.length > 10) &&
+        !username.match(/^[a-zA-Z0-9]{5,10}$/)
+      ) {
+        setusernameError(
+          "User name must be only letters and numbers and 5-10 characters long!"
+        );
+      } else if (!username.match(/^[a-zA-Z0-9]{5,10}$/)) {
+        setusernameError("User name must be only letters and numbers !");
+      } else {
+        setusernameError("User name must be 5-10 characters long!");
+      }
     } else {
+      formIsValid = true;
       setusernameError("");
     }
   }
+
   function Checkpassword(password) {
-    setForminvalid(false);
     setPassword(password);
-    if (!password.match(/^[a-zA-Z0-9@#$%^&*()]{8,22}$/)) {
-      setpasswordError(
-        "Only numbers, letters and @ # $ % ^ & and length between 8 and 22"
-      );
+    if (
+      !password.match(/^[a-zA-Z0-9@#$%^&*?()]{8,22}$/) ||
+      password.length < 8 ||
+      password.length > 22
+    ) {
+      formIsValid = false;
+      if (
+        !password.match(/^[a-zA-Z0-9@#$%^&*?()]{8,22}$/) &&
+        (password.length < 8 || password.length > 22)
+      ) {
+        setpasswordError(
+          "Password must be only numbers, letters or @ # $ % ^ & * ? between 8-22 characters long"
+        );
+      } else if (!password.match(/^[a-zA-Z0-9@#$%^&*?()]{8,22}$/)) {
+        setpasswordError("Only numbers, letters or @ # $ % ^ & * ?) ( ");
+      } else {
+        setpasswordError("Password must be 8-22 characters long");
+      }
     } else {
       setpasswordError("");
     }
@@ -47,81 +115,111 @@ function Loginapp() {
 
   const loginSubmit = (e) => {
     e.preventDefault();
-    console.log("handlevalidation", handleValidation(e));
-    if (handleValidation(e) === true) {
-      // alert(`form valid ${username}, ${password}`);
-      handleValidation(e);
-      setFormsubmitted(true);
-      setUsername("");
-      setPassword("");
-      setTimeout(() => setFormsubmitted(false), 1000);
-      console.log(username, password);
+    handleValidation(e);
+    // saveuser(username, password);
+
+    if (formIsValid === true) {
+      saveuser(username, password);
     } else {
-      setFormsubmitted(false);
-      //setForminvalid(true);
       console.log("form invalid");
     }
   };
 
   return (
     <div className="App">
-      {/*<div className="container">
-        {/*<div className="row d-flex justify-content-center"> */}
-        {/*<div className="col-md-4"> */}
-          <form id="loginform" onSubmit={loginSubmit}>
-            {formsubmitted && (
-              <p style={{ color: "yellow" }}>Form submitted successfully!</p>
-            )}
-            {forminvalid && (
-              <p style={{ color: "yellow" }}>
-                Make sure your username and password meet the requirements!
-              </p>
-            )}
-            <div className="form-group">
-              <label style={{ color: "white" }}>Username</label>
-              <input
-                type="text"
-                className="form-control m-2"
-                id="Username"
-                name="Username"
-                placeholder="Enter username"
-                value={username}
-                onChange={(e) => {
-                  Checkusername(e.target.value);
-                }}
-              />
-              <small id="emailHelp" className="text-danger form-text">
-                {usernameError}
-              </small>
-            </div>
+      <form id="loginform" onSubmit={loginSubmit}>
+        <div
+          style={{
+            color: "yellow",
+            fontSize: "1.5em",
+            /* */
+          }}
+        ></div>
 
-            <div className="form-group">
-              <label style={{ color: "white" }}>Password</label>
-              <br />
+        {userexists && (
+          <div
+            style={{
+              boxShadow: "rgba(0, 0, 0, 0.35) 0px 5px 15px",
+              padding: "1rem",
+              borderRadius: "0.5rem",
+              backgroundColor: "rgb(254, 98, 98)",
+              margin: "0.5rem",
+            }}
+            id="status"
+          >
+            User already exists!
+          </div>
+        )}
+        {usernotexists && (
+          <div
+            style={{
+              backgroundColor: "rgb(254, 98, 98)",
+              boxShadow: "rgba(0, 0, 0, 0.35) 0px 5px 15px",
+              padding: "1rem",
+              borderRadius: "0.5rem",
+              margin: "0.5rem",
+            }}
+            id="status"
+          >
+            User created successfully!
+          </div>
+        )}
+        <div className="form-group">
+          <label
+            style={{
+              color: "black",
+              textShadow:
+                "rgba(0, 0, 0, 0.16) 0px 3px 6px, rgba(0, 0, 0, 0.23) 0px 3px 6px",
+            }}
+          >
+            Username
+          </label>
+          <input
+            type="text"
+            className="form-control m-2"
+            id="Username"
+            name="Username"
+            placeholder="Enter username"
+            value={username}
+            onChange={(e) => {
+              Checkusername(e.target.value);
+            }}
+          />
+          <small id="usernameerror" className="text-danger form-text">
+            {usernameError}
+          </small>
+        </div>
+        <div id="passworderror" className="form-group">
+          <label
+            style={{
+              color: "black",
+              textShadow:
+                "rgba(0, 0, 0, 0.16) 0px 3px 6px, rgba(0, 0, 0, 0.23) 0px 3px 6px",
+            }}
+          >
+            Password
+          </label>
 
-              <input
-                type="password"
-                className="form-control m-2"
-                id="exampleInputPassword1"
-                placeholder="Password"
-                value={password}
-                onChange={(e) => {
-                  Checkpassword(e.target.value);
-                }}
-              />
-            </div>
-            <div>
-              <small id="passworderror" className="text-danger form-text">
-                {passwordError}
-              </small>
-            </div>
-            <button type="submit" className="btn btn-primary">
-              Create new user
-            </button>
-          </form>
-       {/*} </div>
-        {/*</div>*/}
-      {/*</div> */}
+          <input
+            type="password"
+            className="form-control m-2"
+            id="exampleInputPassword1"
+            placeholder="Password"
+            value={password}
+            onChange={(e) => {
+              Checkpassword(e.target.value);
+            }}
+          />
+        </div>
+        <div>
+          <small id="passworderror" className="text-danger form-text">
+            {passwordError}
+          </small>
+        </div>
+        <button type="submit" className="btn btn-primary">
+          Create new user
+        </button>
+      </form>
     </div>
   );
 }

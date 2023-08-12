0cd63f0795ade61b9444fba662ecf28f53e6ada0
hmon <wu2504hm-s@student.lu.se>
2023-08-13 00:38:40 +0200

    added function to summary button

--- client/src/admin_panel.js
+++ client/src/admin_panel.js
@@ -1,6 +1,7 @@
 //name the personalised tracker
 //add task/edit tasks
 import { useState, useEffect } from "react";
+import Button from "react-bootstrap/Button";
 
 import Postrequest from "./postrequest";
 import Getrequest from "./getrequest";
@@ -10,8 +11,10 @@ const username = "default"
     const[data,setData] = useState("")
     const [identifier,setIdentifier] = useState("")
 
-async function getall(){
-  Getrequest("http://192.168.0.6:3000/todos")
+async function checkidentifier(){
+  if (!identifier){
+    
+  }
 }
 const url = `http://192.168.0.6:3000/addidentifier/${username}`
 console.log(url)
@@ -24,38 +27,83 @@ const loginSubmit = (e) => {
     e.preventDefault();
     addidentifier(identifier);
   };
+let x;
+useEffect(()=>{
+   // Getrequest(`http://192.168.0.6:3000/todo/${identifier}`).then(data =>  {console.log(typeof(data));console.log(data.values)})//data.map((x)=>{ console.log(x.description); console.log(x.hmon_count)}))
+;
+
+},[])
 
 
 return (
   <>
-     <form id="loginform" onSubmit={loginSubmit}>
-      <div className="form-group">
-        <label
-          style={{
-            color: "black",
-            textShadow:
-              "rgba(0, 0, 0, 0.16) 0px 3px 6px, rgba(0, 0, 0, 0.23) 0px 3px 6px",
-          }}
-        >
-          Tracker nickname
-        </label>
-        <input
-          type="text"
-          className="form-control m-2"
-          id="Username"
-          name="Username"
-          placeholder="Enter username"
-          value={identifier}
-          onChange={(e) => {
-            setIdentifier(e.target.value);
-          }}
-        />
-        <small id="usernameerror" className="text-danger form-text"></small>
-      </div>
-      <button type="submit" className="btn btn-primary">
-        Submit
-      </button>
-    </form>
+    {!identifier && (
+      <form id="loginform" onSubmit={loginSubmit}>
+        <div className="form-group">
+          <label
+            style={{
+              color: "black",
+              textShadow:
+                "rgba(0, 0, 0, 0.16) 0px 3px 6px, rgba(0, 0, 0, 0.23) 0px 3px 6px",
+            }}
+          >
+            Tracker nickname
+          </label>
+          <input
+            type="text"
+            className="form-control m-2"
+            id="Username"
+            name="Username"
+            placeholder="Enter username"
+            value={identifier}
+            onChange={(e) => {
+              setIdentifier(e.target.value);
+            }}
+          />
+          <small id="usernameerror" className="text-danger form-text"></small>
+        </div>
+        <Button type="submit" className="btn btn-primary">
+          Submit
+        </Button>
+      </form>
+    )}
+    <div className="form-group">
+      <label
+        style={{
+          color: "black",
+          textShadow:
+            "rgba(0, 0, 0, 0.16) 0px 3px 6px, rgba(0, 0, 0, 0.23) 0px 3px 6px",
+        }}
+      >
+        New chore name
+      </label>
+      <input
+        type="text"
+        className="form-control m-2"
+        id="Username"
+        name="Username"
+        placeholder="Enter username"
+        value={identifier}
+        onChange={(e) => {
+          setIdentifier(e.target.value);
+        }}
+      />
+      <small id="usernameerror" className="text-danger form-text"></small>
+    </div>
+    <Button type="submit" className="btn btn-primary">
+      Add new chore
+    </Button>
+    <Button
+      onClick={()=>{Getrequest(`http://192.168.0.6:3000/todo/${identifier}`).then(
+        (data) => {
+          console.log(typeof data);
+          console.log(data[0]);
+        }
+      )}}
+      className="btn btn-primary"
+    >
+      Summary
+    </Button>
   </>
 );
 

3eefadf97d10c217b2093fd143c4cc380d0f0c76
hmon <wu2504hm-s@student.lu.se>
2023-08-13 06:21:41 +0200

    change todo/:id path to todo/id/:id

--- index.js
+++ index.js
@@ -302,20 +302,20 @@ app.get("/todo", async (req, res) => {
   }
 });
 
-// app.get("/todo/:id", async (req, res) => {
-//   try {
+app.get("/todo/id/:id", async (req, res) => {
+  try {
 
-//     const { id } = req.params;
-//     console.log(id)
-//     const specific_todo = await pool.query(
-//       "SELECT * FROM todo WHERE todo_id = $1 ",
-//       [id]
-//     );
-//     res.json(specific_todo.rows[0]);
-//   } catch (error) {
-//     console.error(error.message);
-//   }
-// });
+    const { id } = req.params;
+    console.log(id)
+    const specific_todo = await pool.query(
+      "SELECT * FROM todo WHERE todo_id = $1 ",
+      [id]
+    );
+    res.json(specific_todo.rows[0]);
+  } catch (error) {
+    console.error(error.message);
+  }
+});
 
 app.get("/todo/:identifier", async (req, res) => {
   try {

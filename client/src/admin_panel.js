ed00a288b6e00e97a67c24812b38389a92e38ace
hmon <wu2504hm-s@student.lu.se>
2023-08-13 05:23:12 +0200

    refactored

--- client/src/admin_panel.js
+++ client/src/admin_panel.js
@@ -5,108 +5,129 @@ import Button from "react-bootstrap/Button";
 
 import Postrequest from "./postrequest";
 import Getrequest from "./getrequest";
-import url from "./url"
+import url from "./url";
+import css from "./index.css";
+import Summary from "./summary";
 
 const AdminPanel = () => {
-const username = "default"
-    const[data,setData] = useState("")
-    const [identifier,setIdentifier] = useState("")
+  const username = "Default";
+  const [data, setData] = useState("");
+  const [identifier, setIdentifier] = useState("Default tracker");
+  const [list, setList] = useState([]);
+  //////////replace " " with hypens
+  const { newchorename, setNewchorename } = useState("");
 
-async function checkidentifier(){
-  if (!identifier){
-    
+  async function checkidentifier() {
+    if (!identifier) {
+    }
   }
-}
-const newurl = `${url}/addidentifier/${username}`
-console.log(newurl)
-async function addidentifier (identifier){
-const body = {identifier: `'${identifier}'`}
-    Postrequest (newurl, body).then(data=> console.log(data.message))}
+  const newurl = `${url}/addidentifier/${username}`;
+  console.log(newurl);
+  async function addidentifier(identifier) {
+    const body = {
+      identifier: `'${identifier.replace(/\s+/g, "-").toLowerCase()}'`,
+    };
+    Postrequest(newurl, body).then((data) => console.log(data.message));
+  }
+  let tmp_list = [];
+  async function getsummary() {
+    Getrequest(`${url}/todo/${identifier.replace(/\s+/g, "-").toLowerCase()}`)
+      .then((data) => {
+        console.log(typeof data);
+        console.log(data[0]);
+        tmp_list.push(data);
+        return tmp_list; // Returning the updated tmp_list
+      })
+      .then((updatedList) => {
+        setList(updatedList); // Update the state with the updated list
+        console.log(updatedList); // Logging the updated list
+      });
+  }
 
-const loginSubmit = (e) => {
+  const loginSubmit = (e) => {
     e.preventDefault();
     addidentifier(identifier);
   };
-let x;
-useEffect(()=>{
-   // Getrequest(`${url}/todo/${identifier}`).then(data =>  {console.log(typeof(data));console.log(data.values)})//data.map((x)=>{ console.log(x.description); console.log(x.hmon_count)}))
-;
 
-},[])
+  useEffect(() => {
+    getsummary();
+  }, []);
 
+  return (
+    <>
+      {" "}
+      {identifier && <div id="tracker-name">{identifier}</div>}
+      <div id="admin-panel">
+        {!identifier && (
+          <form id="loginform" onSubmit={loginSubmit}>
+            <div className="input">
+              <label
+                style={{
+                  color: "black",
+                  textShadow:
+                    "rgba(0, 0, 0, 0.16) 0px 3px 6px, rgba(0, 0, 0, 0.23) 0px 3px 6px",
+                }}
+              >
+                Tracker nickname
+              </label>
+              <input
+                type="text"
+                className="form-control m-2"
+                id="Username"
+                name="Username"
+                placeholder="Enter username"
+                value={identifier}
+                onChange={(e) => {
+                  setIdentifier(e.target.value);
+                }}
+              />
+              <small
+                id="usernameerror"
+                className="text-danger form-text"
+              ></small>
 
-return (
-  <>
-    {!identifier && (
-      <form id="loginform" onSubmit={loginSubmit}>
-        <div className="form-group">
+              <Button type="submit" className="btn btn-primary">
+                Submit
+              </Button>
+            </div>
+          </form>
+        )}
+        <div className="input">
           <label
             style={{
               color: "black",
               textShadow:
                 "rgba(0, 0, 0, 0.16) 0px 3px 6px, rgba(0, 0, 0, 0.23) 0px 3px 6px",
             }}
-          >
-            Tracker nickname
-          </label>
-          <input
+          ></label>
+          {/* <input
             type="text"
             className="form-control m-2"
             id="Username"
             name="Username"
-            placeholder="Enter username"
-            value={identifier}
+            placeholder="New chore name"
+            value={newchorename}
             onChange={(e) => {
-              setIdentifier(e.target.value);
+              setNewchorename(e.target.value);
             }}
           />
-          <small id="usernameerror" className="text-danger form-text"></small>
+          <small id="usernameerror" className="text-danger form-text"></small>{" "} */}
+          <Button type="submit" className="btn btn-primary">
+            Add new chore
+          </Button>
         </div>
-        <Button type="submit" className="btn btn-primary">
-          Submit
+        <Button
+          onClick={(e) => {
+            console.log(e.target);
+          }}
+          className="btn btn-primary"
+        >
+          Change chore name
         </Button>
-      </form>
-    )}
-    <div className="form-group">
-      <label
-        style={{
-          color: "black",
-          textShadow:
-            "rgba(0, 0, 0, 0.16) 0px 3px 6px, rgba(0, 0, 0, 0.23) 0px 3px 6px",
-        }}
-      >
-        New chore name
-      </label>
-      <input
-        type="text"
-        className="form-control m-2"
-        id="Username"
-        name="Username"
-        placeholder="Enter username"
-        value={identifier}
-        onChange={(e) => {
-          setIdentifier(e.target.value);
-        }}
-      />
-      <small id="usernameerror" className="text-danger form-text"></small>
-    </div>
-    <Button type="submit" className="btn btn-primary">
-      Add new chore
-    </Button>
-    <Button
-      onClick={()=>{Getrequest(`${url}/todo/${identifier}`).then(
-        (data) => {
-          console.log(typeof data);
-          console.log(data[0]);
-        }
-      )}}
-      className="btn btn-primary"
-    >
-      Summary
-    </Button>
-  </>
-);
-
-}
+      </div>
+      <Summary list={list} />
+    </>
+  );
+};
 
 export default AdminPanel;

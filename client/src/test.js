e81b7d919d7245791f5f47bb35681c96427534f4
hmon <wu2504hm-s@student.lu.se>
2023-08-14 13:46:45 +0200

    temp added test component

---
+++ client/src/test.js
@@ -0,0 +1,43 @@
+import { useState, createContext, useEffect } from "react";
+import AdminPanel from "./admin_panel";
+import Summary from "./summary";
+import url from "./url";
+import Getrequest from "./getrequest";
+
+const ListContext = createContext();
+const username = "default";
+const identifier = "default tracker";
+const newurl = `${url}/addidentifier/${username}`;
+const newurl2 = `${url}/todo/${identifier.replace(/\s+/g, "-").toLowerCase()}`;
+console.log("newurl2", newurl2);
+
+function Component() {
+  const [list, setList] = useState([]);
+  async function getsummary(url) {
+    let tmp_list = [];
+    Getrequest(url)
+      .then((data) => {
+        console.log(typeof data);
+        console.log(data[0]);
+        tmp_list.push(data);
+        return tmp_list; // Returning the updated tmp_list
+      })
+      .then((updatedList) => {
+        setList(updatedList); // Update the state with the updated list
+        console.log(updatedList); // Logging the updated list
+      });
+  }
+  useEffect(() => {
+    console.log("url in getsummary ", newurl2);
+    getsummary(newurl2);
+    // console.log("list from getsummary", list);
+  }, []);
+  return (
+    <ListContext.Provider value={list}>
+      <AdminPanel list={list} />
+      <Summary list={list} />
+    </ListContext.Provider>
+  );
+}
+
+export default Component;

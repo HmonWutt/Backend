b99854f681b570b50d54032d29685ef7f9da7006
hmon <wu2504hm-s@student.lu.se>
2023-08-15 15:59:33 +0200

    decoupled navbar from root component

--- client/src/root.js
+++ client/src/root.js
@@ -1,77 +1,12 @@
 import { Outlet, Link, NavLink } from "react-router-dom";
+import { NavBar } from "./navbar";
 
 export default function Root() {
-  //   const links = document.querySelectorAll(".link");
-
-  //   console.log(links);
-  //   const handleclick = (e) => {
-  //     const target = e.target.getAttribute("id");
-  //     for (const link of links) {
-  //       link.style.color = "rgb(139, 0, 139)";
-  //       if (link.getAttribute("id") !== target) {
-  //         link.style.color = "blue";
-  //       }
-  //     }
-  //   };
   return (
     <>
-      <div id="sidebar">
-        {/* other elements */}
-
-        <nav>
-          <ul id="navbaritems">
-            <li>
-              <NavLink
-                id="home-link"
-                to="/home"
-                className={({ isActive, isPending }) =>
-                  isPending ? "pending" : isActive ? "active" : ""
-                }
-              >
-                Demo
-              </NavLink>
-            </li>
-            <li>
-              <NavLink
-                id="create-link"
-                to="/createaccount"
-                className={({ isActive, isPending }) =>
-                  isPending ? "pending" : isActive ? "active" : ""
-                }
-              >
-                Create account
-              </NavLink>
-            </li>
-            <li>
-              <NavLink
-                id="admin-link"
-                to="/adminpanel"
-                className={({ isActive, isPending }) =>
-                  isPending ? "pending" : isActive ? "active" : ""
-                }
-              >
-                Admin panel
-              </NavLink>
-            </li>
-            <li>
-              <NavLink
-                id="component-link"
-                to="/component"
-                className={({ isActive, isPending }) =>
-                  isPending ? "pending" : isActive ? "active" : ""
-                }
-              >
-                Test component
-              </NavLink>
-            </li>
-          </ul>
-        </nav>
-      </div>
-
+      <NavBar />
       {/* other elements */}
-      <div id="detail">
-        <Outlet />
-      </div>
+      <Outlet />
     </>
   );
 }

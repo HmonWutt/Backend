d121f2b9b465a75191e59752ff610faea8cb489e
hmon <wu2504hm-s@student.lu.se>
2023-08-15 03:13:16 +0200

    handle unauthorised if not logged in

--- client/src/home.js
+++ client/src/home.js
@@ -1,20 +1,28 @@
 import { useState, useEffect } from "react";
+import { NavLink } from "react-router-dom";
 import Button from "react-bootstrap/Button";
 import Taskselector from "./taskselector";
 import "./index.css";
-import url from "./url"
+import url from "./url";
 const Home = () => {
   const [descriptions, setDescriptions] = useState([]);
   const [taskid, setTaskid] = useState("");
   const [taskshow, settaskshow] = useState(false);
+  const [isLoggedIn, setIsLoggedIn] = useState(true);
 
   const Gettodo = async () => {
     try {
-      const response = await fetch(`${url}/descriptions`, {
+      const response = await fetch(`${url}/todo/descriptionlist/descriptions`, {
         method: "GET",
         headers: { "Content-Type": "application/json" },
       });
       let data = await response.json();
+      if (data.message !== "success") setIsLoggedIn(false);
+      else {
+        setDescriptions(data);
+        descriptions && console.log(descriptions);
+      }
+
       setDescriptions(data);
       descriptions && console.log(descriptions);
 
@@ -31,20 +39,26 @@ const Home = () => {
   return (
     <>
       <div className="gap-2">
-        {descriptions.map((description, index) => (
-          <Button
-            onClick={() => {
-              console.log(description.todo_id);
-              setTaskid(description.todo_id);
-              settaskshow(true);
-            }}
-            className="mt-2 me-2"
-            key={index}
-            variant="warning"
-          >
-            {description.description}{" "}
-          </Button>
-        ))}
+        {isLoggedIn ? (
+          descriptions.map((description, index) => (
+            <Button
+              onClick={() => {
+                console.log(description.todo_id);
+                setTaskid(description.todo_id);
+                settaskshow(true);
+              }}
+              className="mt-2 me-2"
+              key={index}
+              variant="warning"
+            >
+              {description.description}{" "}
+            </Button>
+          ))
+        ) : (
+          <div>
+            Please <NavLink to="/login">log in</NavLink> to access this page.
+          </div>
+        )}
       </div>
       {taskshow && <Taskselector taskid={taskid} />}
     </>

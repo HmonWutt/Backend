16d855acfde7a809bbedff676017bf78d7f79ebf
hmon <wu2504hm-s@student.lu.se>
2023-08-15 16:42:21 +0200

    changed from using state to dataloader

--- client/src/test.js
+++ client/src/test.js
@@ -3,6 +3,7 @@ import AdminPanel from "./admin_panel";
 import Summary from "./summary";
 import url from "./url";
 import Getrequest from "./getrequest";
+import { useLoaderData } from "react-router-dom";
 
 const ListContext = createContext();
 const username = "default";
@@ -11,28 +12,10 @@ const newurl = `${url}/addidentifier/${username}`;
 const newurl2 = `${url}/todo/${identifier.replace(/\s+/g, "-").toLowerCase()}`;
 console.log("newurl2", newurl2);
 
-function Component() {
-  const [list, setList] = useState([]);
+export function Component() {
    const [isHidden, setIsHidden] = useState(true);
-  async function getsummary(url) {
-    let tmp_list = [];
-    Getrequest(url)
-      .then((data) => {
-        console.log(typeof data);
-        console.log(data[0]);
-        tmp_list.push(data);
-        return tmp_list; // Returning the updated tmp_list
-      })
-      .then((updatedList) => {
-        setList(updatedList); // Update the state with the updated list
-        console.log(updatedList); // Logging the updated list
-      });
-  }
-  useEffect(() => {
-    console.log("url in getsummary ", newurl2);
-    getsummary(newurl2);
-    // console.log("list from getsummary", list);
-  }, []);
+  const list = useLoaderData()
+
   return (
     <ListContext.Provider value={{ list, isHidden, setIsHidden }}>
       <Summary list={list} isHidden={isHidden} setIsHidden={setIsHidden} />
@@ -42,4 +25,16 @@ function Component() {
   );
 }
 
-export default Component;
+export const dataLoader = async()=>{
+  let tmp_list = [];
+  Getrequest(newurl2)
+    .then((data) => {
+      console.log(typeof data);
+      console.log(data[0]);
+      tmp_list.push(data);
+      console.log("tmp",tmp_list)
+     
+      // Returning the updated tmp_list
+    })
+    return tmp_list; 
+}

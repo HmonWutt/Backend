df72e0b7dd5e07d6751eb7c886a009cefd2cba13
hmon <wu2504hm-s@student.lu.se>
2023-08-15 15:57:39 +0200

    Created App.js-router

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,37 @@
-
+import {
+  BrowserRouter,
+  createBrowserRouter,
+  RouterProvider,
+  Routes,
+  Route,
+  createRoutesFromElements,
+} from "react-router-dom";
 import './App.css';
+import Root from "./root";
+import Loginapp from "./loginapp";
+import Createuserapp from "./createuser";
+import Home from "./home";
+import Component from "./test";
+import { NavBar } from "./navbar";
 
-function App() {
+function Apps() {
+
+  const router = createBrowserRouter(createRoutesFromElements(
+    <Route path="/" element={<Root/>}>
+      <Route index element={<Loginapp/>}/>
+      <Route path="/register" element={<Createuserapp/>}/>
+      <Route path="/home" element={<Home/>}/>
+      <Route path="/component" element={<Component/>}/>
+    </Route> 
+    
+     ))
+
   return (
-<></>
+    
+      <RouterProvider router={router}/>
+
+
   );
 }
 
-export default App;
+export default Apps;

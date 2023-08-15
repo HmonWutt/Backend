671bab072e052d70370fc93bb561b0091cbff963
hmon <wu2504hm-s@student.lu.se>
2023-08-15 03:17:01 +0200

    added ishidden

--- client/src/summary.js
+++ client/src/summary.js
@@ -1,20 +1,24 @@
-import { useEffect } from "react";
+import { useState, useEffect } from "react";
 import Button from "react-bootstrap/Button";
 
-function Summary({list}) {
-  console.log("summary runs")
+function Summary({ list , isHidden, setIsHidden}) {
+  console.log("summary runs");
+
 
   // list.forEach((element) => {
   //   element.map((task, index) => console.log(task));
   // });
-
+  const handleclick = (e) => {
+    //document.getElementById("summary").classList.add("hidden")
+    setIsHidden(!isHidden);
+  };
   useEffect(() => {
     console.log("summary use effect runs");
     const cards = document.getElementsByClassName("card");
     const cardArray = Array.from(cards);
     console.log("cards", cards);
     console.log("cardarry", cardArray);
-   
+
     document.getElementById("cards").onmousemove = (e) => {
       for (const card of cardArray) {
         const rect = card.getBoundingClientRect();
@@ -30,19 +34,33 @@ function Summary({list}) {
   //
   return (
     <>
-      <Button>Summary</Button>
-      <div id="cards">
-        {list.map((element, elementIndex) =>
-          element.map((task, taskIndex) => (
-            <div key={taskIndex} className="card">
-              <div className="card-content">
-                {task.description}
-                {task.hmon_count}
-                {task.joakim_count}
+      <div
+        id="summary"
+        class=""
+        style={{
+          backgroundColor: isHidden ? "rgb(250, 148, 148)" : "rgb(30, 30, 30)",
+        }}
+      >
+        <Button onClick={handleclick}>
+          {isHidden ? "Show summary" : "Close"}
+        </Button>
+        <div
+          id="cards"
+          className=""
+          style={{ visibility: isHidden ? "hidden" : "visible" }}
+        >
+          {list.map((element, elementIndex) =>
+            element.map((task, taskIndex) => (
+              <div key={taskIndex} className="card">
+                <div className="card-content">
+                  {task.description}
+                  {task.hmon_count}
+                  {task.joakim_count}
+                </div>
               </div>
-            </div>
-          ))
-        )}
+            ))
+          )}
+        </div>
       </div>
     </>
   );

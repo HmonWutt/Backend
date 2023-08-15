c95247c475ef39e8a1b5c428bf42b8d96ff2ce9f
hmon <wu2504hm-s@student.lu.se>
2023-08-15 16:07:46 +0200

    cleanup

--- client/src/index.js
+++ client/src/index.js
@@ -1,81 +1,90 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
+
 import reportWebVitals from "./reportWebVitals";
 import "bootstrap/dist/css/bootstrap.min.css";
 
 import "./index.css";
-
-import Root from "./root";
-import Tasklist from "./tasklist";
-import Createuserapp from "./createuser";
-import Task from "./task";
-import Home from "./home";
-import ErrorPage from "./errorhandler";
-import Loginapp from "./loginapp";
-//import {AdminPanel} from "./admin_panel";
-import Component from "./test";
-
-const router = createBrowserRouter([
-  {
-    path: "/createaccount",
-
-    element: (
-      <>
-        <Root />
-        <Createuserapp />
-      </>
-    ),
-    errorElement: <ErrorPage />,
-  },
-  {
-    path: "home",
-    element: (
-      <>
-        <Root />
-        <Home />
-      </>
-    ),
-    errorElement: <ErrorPage />,
-  },
-  {
-    path: "login",
-    element: (
-      <>
-        <Root />
-        <Loginapp />
-      </>
-    ),
-  },
-  {
-    /*
-    path: "adminpanel",
-    element: (
-      <>
-        <Root />
-        <AdminPanel />
-      </>
-    ),
-    */
-  },
-  {
-    path: "component",
-    element: (
-      <>
-        <Root />
-        <Component />
-      </>
-    ),
-  },
-]);
+import Apps from "./App";
 
 document.body.style.backgroundColor = `#dcdcdc`;
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   // <React.StrictMode>
-  <RouterProvider router={router} />
+  <Apps />
+
+  //<RouterProvider router={router} />
   // </React.StrictMode>
 );
+///-----------------------------------------------------------------------------------------------------////
+// import {
+//   BrowserRouter,
+//   createBrowserRouter,
+//   RouterProvider,
+//   Routes,
+//   Route,
+//   createRoutesFromElements,
+// } from "react-router-dom";
+
+// import Root from "./root";
+// import Tasklist from "./tasklist";
+// import Createuserapp from "./createuser";
+// import Task from "./task";
+// import Home from "./home";
+// import ErrorPage from "./errorhandler";
+// import Loginapp from "./loginapp";
+// import Component from "./test";
+
+//  const router = createBrowserRouter([
+//   {
+//     path: "/",
+
+//     element: (
+//       <>
+//         <Root />
+//       </>
+//     ),
+//     errorElement: <ErrorPage />,
+//     children: [
+//       {
+//         index: true,
+//         element: (
+//           <>
+//             <Loginapp />
+//           </>
+//         ),
+//       },
+//       {
+//         path: "/createaccount",
+
+//         element: (
+//           <>
+//             <Createuserapp />
+//           </>
+//         ),
+//         errorElement: <ErrorPage />,
+//       },
+//       {
+//         path: "home",
+//         element: (
+//           <>
+//             <Home />
+//           </>
+//         ),
+//         errorElement: <ErrorPage />,
+//       },
+
+//       {
+//         path: "component",
+//         element: (
+//           <>
+//             <Component />
+//           </>
+//         ),
+//       },
+//     ],
+//   },
+//]
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))

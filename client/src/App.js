d2f5a6cbdf42849908dae1822dc80914f49b481e
hmon <wu2504hm-s@student.lu.se>
2023-08-16 14:07:05 +0200

    clean up

--- client/src/App.js
+++ client/src/App.js
@@ -9,18 +9,32 @@ import "./App.css";
 import Root from "./root";
 import Loginapp from "./loginapp";
 import Createuserapp from "./createuser";
+import { Homedata, descriptionLoader } from "./homedata";
 import Home from "./home";
 import { Component, dataLoader } from "./test";
+import ProtectedRoutes from "./Protectedroute";
 
 function Apps() {
   const router = createBrowserRouter(
     createRoutesFromElements(
-      <Route path="/" element={<Root />}>
-        <Route index element={<Loginapp />} />
-        <Route path="/register" element={<Createuserapp />} />
-        <Route path="/home" element={<Home />} />
-        <Route path="/component" element={<Component />} loader={dataLoader} />
-      </Route>
+      <>
+        <Route path="/" element={<Root />}>
+          <Route index element={<Loginapp />} />
+          <Route path="/register" element={<Createuserapp />} />
+          <Route
+            path="/home"
+            element={<Homedata />}
+            loader={descriptionLoader}
+          />
+          <Route element={<ProtectedRoutes />}>
+            <Route
+              path="/component"
+              element={<Component />}
+              loader={dataLoader}
+            />
+          </Route>
+        </Route>
+      </>
     )
   );
 

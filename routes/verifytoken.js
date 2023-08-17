3fa113bbf0a924e739dc11b5a235cd45b571d6e3
hmon <wu2504hm-s@student.lu.se>
2023-08-17 05:57:30 +0200

    edited token parsing

--- routes/verifytoken.js
+++ routes/verifytoken.js
@@ -1,17 +1,18 @@
-const jwt = require("jsonwebtoken")
-const {secret} = require("./secret")
+const jwt = require("jsonwebtoken");
+const { secret } = require("./secret");
 
-module.exports = function (req,res,next) {
-    const token = req.header('auth-token')
-    console.log("verifying",token)
-    if (!token)return res.status(401).send({message:"access denied!"})
-    try {
-        const verified = jwt.verify(token,secret)
-        req.username = verified
-        console.log(req.username)
-        next()
-    } catch (error) {
-        res.status(400).send({message:"Invalid token"})
-        
-    }
-}
+module.exports = function (req, res, next) {
+  console.log(req.header);
+  const authHeader = req.headers["authorization"];
+  const token = authHeader && authHeader.split(" ")[1];
+  console.log("verifying", token);
+  if (!token) return res.status(401).send({ message: "access denied!" });
+  try {
+    const verified = jwt.verify(token, secret);
+    req.username = verified;
+    console.log(req.username);
+    next();
+  } catch (error) {
+    res.status(400).send({ message: "Invalid token" });
+  }
+};

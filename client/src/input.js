9ecbbda9c33ea853277a6e17ce3a74a9ede5824e
hmon <wu2504hm-s@student.lu.se>
2023-08-23 22:41:12 +0200

    created getinput

--- client/src/input.js
+++ client/src/input.js
@@ -1,101 +1,39 @@
-import Button from "react-bootstrap/Button";
-import Modal from "react-bootstrap/Modal";
-import { useState } from "react";
+import { useRef, useState, useEffect } from "react";
+export default function Getinput({ labeltext, placeholdertext, idtext, next }) {
+  const [value, setValue] = useState("");
+  const ref = useRef();
 
-const Input = ({ gettodos, id }) => {
-  const [input, setinput] = useState("");
-  const [modalshow, setmodalshow] = useState(false);
-  const [submitmodal, setsubmitmodal] = useState(false);
-  const [show, setShow] = useState(false);
-
-  const handleClose = () => setShow(false);
-  const handleShow = () => setShow(true);
-
-  function Isemptystring() {
-    if (!input) {
-      alert("Description cannot be empty!");
-    } else {
-      setmodalshow(true);
-      handleClose();
-    }
-  }
-
-  async function Updatedescription(id) {
-    try {
-      await fetch(`http://192.168.0.6:3000/todo/${id}`, {
-        method: "PUT",
-        headers: { "Content-Type": "application/json" },
-        body: JSON.stringify({ set: `SET description =  '${input}'` }),
-      });
-      setmodalshow(false);
-      gettodos();
-      setsubmitmodal(true);
-      setTimeout(() => {
-        setsubmitmodal(false);
-      }, 1000);
-      return setinput("");
-    } catch (error) {
-      console.error(error.message);
-    }
-  }
-
+  useEffect(() => {
+    next(ref.current.value);
+  }, [value]);
   return (
-    <div>
-      <Button variant="primary" onClick={handleShow} className="m-3">
-        Update description
-      </Button>
-
-      <Modal
-        show={show}
-        onHide={handleClose}
-        className="m-3 d-flex justify-content-center"
+    <div
+      className="input"
+      style={{
+        display: "flex",
+        alignItems: "center",
+        flexDirection: "column",
+      }}
+    >
+      <label
+        style={{
+          color: "black",
+          textShadow:
+            "rgba(0, 0, 0, 0.16) 0px 3px 6px, rgba(0, 0, 0, 0.23) 0px 3px 6px",
+        }}
       >
-        <Modal.Title className="m-2 d-flex justify-content-center">
-          Update description
-        </Modal.Title>
-
-        <Modal.Body>
-          <input
-            type="text"
-            className="m-1"
-            value={input}
-            onChange={(e) => setinput(e.target.value)}
-          ></input>
-          <Button onClick={() => Isemptystring()}>Submit</Button>
-        </Modal.Body>
-      </Modal>
-
-      {modalshow && (
-        <Modal show={true} onHide={() => setmodalshow(false)}>
-          <Modal.Body className="p-5 d-flex justify-content-center text-danger">
-            Are you sure you want to change the task name?
-          </Modal.Body>
-
-          <Modal.Footer className="p-2 d-flex justify-content-center">
-            <Button variant="warning" onClick={() => Updatedescription(id)}>
-              Yes
-            </Button>
-            <Button
-              variant="danger"
-              onClick={() => {
-                setmodalshow(false);
-              }}
-            >
-              No
-            </Button>
-          </Modal.Footer>
-        </Modal>
-      )}
-
-      {submitmodal && (
-        <Modal show={true} onHide={() => setmodalshow(false)}>
-          <Modal.Body className="p-2 d-flex justify-content-center">
-            <h2>Done!</h2>
-          </Modal.Body>
-        </Modal>
-      )}
+        {labeltext}
+      </label>
+      <input
+        type="text"
+        className="form-control m-2"
+        id={idtext}
+        ref={ref}
+        value={value}
+        onChange={(e) => setValue(e.target.value)}
+        placeholder={placeholdertext}
+      />
+      <small id="usernameerror" className="text-danger form-text"></small>
     </div>
   );
-};
-
-export default Input;
+}

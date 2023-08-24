24f1095a0b55becad63af75c710a045c65091b9f
hmon <wu2504hm-s@student.lu.se>
2023-08-25 01:21:36 +0200

    added token to putrequest

--- client/src/postrequest.js
+++ client/src/postrequest.js
@@ -1,8 +1,9 @@
-async function Postrequest(url, body) {
+async function Postrequest(url, body, token) {
   try {
     const result = await fetch(url, {
       method: "POST",
       headers: { "Content-Type": "application/json" },
+      Authorization: `Bearer ${token}`,
       body: JSON.stringify(body),
     });
     var response = await result.json();

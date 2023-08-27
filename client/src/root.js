406fa67bd5a4983ae064ca8da0d4033df6a63080
hmon <wu2504hm-s@student.lu.se>
2023-08-27 08:20:39 +0200

    removed token

--- client/src/root.js
+++ client/src/root.js
@@ -2,18 +2,21 @@ import { Outlet } from "react-router-dom";
 import { useState, createContext } from "react";
 import { NavBar } from "./navbar";
 
-import url from "./url";
-import Postrequest from "./postrequest";
-
 export const Context = createContext("");
 export default function Root() {
   const [username, setUsername] = useState("");
   const [isAuth, setIsAuth] = useState(false);
   const [identifier, setIdentifier] = useState("");
-  const [token, setToken] = useState("");
   const [name1, setName1] = useState("");
   const [name2, setName2] = useState("");
-  console.log("name1,name2 from root", name1, name2);
+  console.log(
+    "name1,name2, username, isAuth, identifier, from root",
+    name1,
+    name2,
+    username,
+    isAuth,
+    identifier
+  );
 
   return (
     <>
@@ -27,8 +30,6 @@ export default function Root() {
           setIsAuth,
           identifier,
           setIdentifier,
-          token,
-          setToken,
           name1,
           setName1,
           name2,

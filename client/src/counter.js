510347437c3db2a973497becae88279ac7ebb93c
hmon <wu2504hm-s@student.lu.se>
2023-08-28 13:45:26 +0200

    modified counter to rid of unnecessary getrequests

--- client/src/counter.js
+++ client/src/counter.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useRef } from "react";
 import Button from "react-bootstrap/Button";
 //import Confettitrigger from "./confetti";
 import ConfettiExplosion from "react-confetti-explosion";
@@ -14,37 +14,56 @@ const Counter = (props) => {
   let todo_id = props.todo_id;
   let identifier = props.identifier;
   const newurl = `${url}/todo/id/${todo_id}/${identifier}`;
-  console.log("counter", newurl);
 
-  const [isExploding, setIsExploding] = useState(false);
+  const [isExploding, setIsExploding] = useState(null);
   const [count, setCount] = useState(0);
   const [name, setName] = useState("");
+  let currentCountRef = useRef(null);
 
-  console.log("counter", todo_id);
-  const Countup = async () => {
-    const body = { set: `SET ${set_name} = ${set_name} + 1` };
-    const confetti = () => {
-      setIsExploding(true);
-      document.getElementById(`${todo_id}`).disabled = true;
-      Getrequest(newurl).then((x) => {
-        setCount(x[set_name]);
+  const disablebButton = (updown, boolean) => {
+    if (updown === "countup") {
+      const disabledButton = document.getElementById(`countup${todo_id}`);
+    } else {
+      const disableButton = document.getElementById(`countdown${todo_id}`);
+    }
+    disablebButton.disabled = boolean;
+    setTimeout(() => {
+      disablebButton.disabled = !boolean;
+    }, 2000);
+  };
+
+  const confetti = (boolean) => {
+    setIsExploding(boolean);
+    setTimeout(() => {
+      setIsExploding(!boolean);
+    }, 2000);
+  };
+
+  const countupbody = { set: `SET ${set_name} = ${set_name} + 1` };
+  const countdownbody = { set: `SET ${set_name} = ${set_name} - 1` };
+  const countzerobody = { set: `SET ${set_name} = 0` };
+
+  const Count = async (body, boolean, updown) => {
+    Putrequest(newurl, body)
+      .then((data) => {
+        console.log(data);
+        if (data.message === "success") {
+          disablebButton(updown, boolean);
+          if (data.count < 0) {
+            Count(countzerobody);
+            setCount(0);
+          } else {
+            setCount(data.count);
+          }
+        } else {
+          console.log(data.message);
+        }
+      })
+      .catch((error) => {
+        console.log("Count update failed", error.message);
       });
-      setTimeout(() => {
-        setIsExploding(false);
-        document.getElementById(`${todo_id}`).disabled = false;
-      }, 2000);
-    };
-
-    Putrequest(newurl, body, confetti);
   };
 
-  const Countdown = async () => {
-    const body = { set: `SET ${set_name} = ${set_name} - 1` };
-    const getrequest = () => {
-      Getrequest(newurl).then((x) => setCount(x[set_name]));
-    };
-    Putrequest(newurl, body, getrequest);
-  };
   useEffect(() => {
     Getrequest(newurl).then((x) => {
       setCount(x[set_name]);
@@ -52,6 +71,13 @@ const Counter = (props) => {
       setName(x[set_name.slice(0, -6)]);
     });
   }, [todo_id]);
+  useEffect(() => {
+    const previousCount = currentCountRef.current;
+    currentCountRef = count;
+    if (currentCountRef > previousCount && currentCountRef > 0) {
+      confetti(true);
+    }
+  }, [count]);
 
   return (
     <>
@@ -89,19 +115,24 @@ const Counter = (props) => {
               ) : */
 
             <Button
-              id={todo_id}
+              id={`countup${todo_id}`}
               className="m-1"
               variant="warning"
-              onClick={Countup}
+              onClick={() => Count(countupbody, true, "countup")}
             >
               Count up
             </Button>
           ) : (
-            <Savetoday countup={Countup} countdown={Countdown} />
+            <Savetoday Count={Count} />
           )}
 
           {todo_id !== 115 ? (
-            <Button className="m-1" variant="dark" onClick={Countdown}>
+            <Button
+              id={`countdown${todo_id}`}
+              className="m-1"
+              variant="dark"
+              onClick={() => Count(countdownbody, false, "countdown")}
+            >
               Undo
             </Button>
           ) : (

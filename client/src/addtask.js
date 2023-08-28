a3f9bd70aaf3896eb59fab09b456972f53e9d6d6
hmon <wu2504hm-s@student.lu.se>
2023-08-28 16:33:22 +0200

    add task works and updates

--- client/src/addtask.js
+++ client/src/addtask.js
@@ -1,37 +1,33 @@
 import Button from "react-bootstrap/Button";
 import Form from "react-bootstrap/Form";
-import { useContext, useEffect, useState } from "react";
+import { useEffect, useState } from "react";
 import Modal from "react-bootstrap/Modal";
 import url from "./url";
-import Getrequest from "./getrequest";
 import Postrequest from "./postrequest";
-import { AddtaskContext } from "./admin_panel";
+import Getinput from "./input";
 
-function Addtask() {
-  const [description, setDescription] = useState("");
-
+function Addtask({ identifier, name1, name2, list, setList, id, setID }) {
   const [show, setShow] = useState(false);
   const handleClose = () => setShow(false);
   const handleShow = () => setShow(true);
   const [isHidden, setIshidden] = useState(null);
 
-  const { identifier, name1, name2, list, setList } =
-    useContext(AddtaskContext);
+  const [description, setDescription] = useState("");
 
   const Submittask = async (e) => {
     e.preventDefault();
-    console.log("name1,name2 from add task", description, name1, name2);
-
     if (description !== "") {
       const body = { description, name1, name2 };
       Postrequest(`${url}/todo/${identifier}`, body)
         .then((data) => {
           if (data.message === "success") {
+            setID(data.id);
             setIshidden(true);
+            setDescription("");
+
             setTimeout(() => {
               setIshidden(null);
-              setDescription("");
-            }, 2000);
+            }, 1000);
           } else {
             setIshidden(false);
           }
@@ -43,59 +39,48 @@ function Addtask() {
       handleShow();
     }
   };
-  function getlist() {
-    Getrequest(
-      `${url}/todo/${identifier.replace(/\s+/g, "-").replace(/'+/g, "")}`
-    ).then((data) => setList(data));
-  }
-  useEffect(() => {
-    getlist();
-  }, [description]);
+  useEffect(() => setID(""));
 
   return (
-    <>
-      <Form className="text-center mt-1">
-        <Form.Group controlId="formBasicEmail">
-          <Form.Control
-            type="text"
-            placeholder="Max 200 characters"
-            value={description}
-            onChange={(e) => {
-              setDescription(e.target.value);
-              setIshidden(null);
-            }}
-          />
-        </Form.Group>
+    <Form className="text-center mt-1">
+      <Form.Group controlId="formBasicEmail">
+        <Form.Control
+          type="text"
+          placeholder="Max 200 characters"
+          value={description}
+          onChange={(e) => {
+            setDescription(e.target.value);
+            setIshidden(null);
+          }}
+        />
+      </Form.Group>
+      <Button
+        variant="dark"
+        type="submit"
+        onClick={Submittask}
+        className="submit"
+        style={{ scale: "0.8" }}
+      >
+        Submit
+      </Button>
 
-        <Button
-          variant="dark"
-          type="submit"
-          onClick={Submittask}
-          className="submit"
-          style={{ scale: "0.8" }}
-        >
-          Submit
-        </Button>
+      {isHidden === true && description === true && (
+        <div>
+          New chore:{" "}
+          <span style={{ color: "blue", fontSize: "1.5em" }}>
+            {description}{" "}
+          </span>
+          added.
+        </div>
+      )}
+      {isHidden === false && <div>Add chore failed. Please try again.</div>}
 
-        {isHidden === true && (
-          <div>
-            New chore:{" "}
-            <span style={{ color: "blue", fontSize: "1.5em" }}>
-              {description}{" "}
-            </span>
-            added.
-          </div>
-        )}
-        {isHidden === false && <div>Add chore failed. Please try again.</div>}
-        <>
-          <Modal show={show} onHide={handleClose}>
-            <Modal.Body className="text-danger ">
-              The input cannot be empty.
-            </Modal.Body>
-          </Modal>
-        </>
-      </Form>
-    </>
+      <Modal show={show} onHide={handleClose}>
+        <Modal.Body className="text-danger ">
+          The input cannot be empty.
+        </Modal.Body>
+      </Modal>
+    </Form>
   );
 }
 

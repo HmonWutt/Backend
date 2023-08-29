2d579b5fd4b4a3e66ca588fa425ca35d51ab87c5
hmon <wu2504hm-s@student.lu.se>
2023-08-29 20:39:34 +0200

    changed font size, fixed up punctuation

--- client/src/barcharts.js
+++ client/src/barcharts.js
@@ -18,28 +18,32 @@ export default function BARChart({
   name1,
   name2,
 }) {
-  const nameone = name1.charAt(0).toUpperCase() + name1.slice(1);
-  const nametwo = name2.charAt(0).toUpperCase() + name2.slice(1);
+  const nameone =
+    name1.charAt(0).toUpperCase() + name1.slice(1).replace(/-/g, " ");
+  const nametwo =
+    name2.charAt(0).toUpperCase() + name2.slice(1).replace(/-/g, " ");
+  const updateddescription =
+    description.charAt(0).toUpperCase() +
+    description.slice(1).replace(/-/g, " ");
 
   return (
     <Bar
-      className="mt-5 col-md-8"
-      style={{ height: "fit-content", width: "fit-content" }}
+      id="barchart"
+      className="mt-5"
+      style={{ height: "fit-content", width: "fit-content", display: "flex" }}
       data={{
         //labels: [taskNames[(taskIndex += 1)]],
-        labels: [nameone, name2],
+        labels: [nameone, nametwo],
 
         datasets: [
           {
-            title: description,
-            label: description,
+            title: updateddescription,
+            //label: updateddescription,
             data: [count1, count2],
             barPercentage: 0.5,
             categoryPercentage: 1,
             backgroundColor: ["#edff63", "#63ffd5"],
             color: "white",
-            width: "100%",
-            height: "100%",
           },
         ],
       }}
@@ -47,15 +51,25 @@ export default function BARChart({
         options: {
           maintainAspectRatio: false,
           plugins: {
+            title: {
+              display: true,
+              text: updateddescription,
+              font: {
+                size: 16,
+                family: "vazir",
+                color: "red",
+              },
+            },
             legend: {
               display: false,
             },
+
             label: {
               display: false,
             },
             layout: {
               padding: {
-                left: 50,
+                left: 0,
                 right: 0,
                 top: 0,
                 bottom: 0,
@@ -74,12 +88,16 @@ export default function BARChart({
             },
           },
           x: {
-            display: true,
             grid: {
               display: false,
             },
             ticks: {
-              display: true, // Remove x-axis ticks
+              display: true,
+              font: {
+                size: 10,
+                color: "",
+              },
+              // Remove x-axis ticks
             },
           },
         },

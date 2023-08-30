a84196fa13ead6bfa8a25a27a037e9d2a4d89ddc
hmon <wu2504hm-s@student.lu.se>
2023-08-30 11:46:30 +0200

    added tick svg

---
+++ client/src/svg.js
@@ -0,0 +1,48 @@
+export const svg = (
+  <svg
+    width="135px"
+    height="135px"
+    viewBox="-2.4 -2.4 28.80 28.80"
+    xmlns="http://www.w3.org/2000/svg"
+    fill="#000000"
+  >
+    <g
+      id="SVGRepo_bgCarrier"
+      stroke-width="0"
+      transform="translate(0,0), scale(1)"
+    >
+      <rect
+        x="-2.4"
+        y="-2.4"
+        width="28.80"
+        height="28.80"
+        rx="1.44"
+        fill="#7eeca5"
+        strokewidth="0"
+      ></rect>
+    </g>
+    <g
+      id="SVGRepo_tracerCarrier"
+      stroke-linecap="round"
+      stroke-linejoin="round"
+    ></g>
+    <g id="SVGRepo_iconCarrier">
+      {" "}
+      <title></title>{" "}
+      <g id="Complete">
+        {" "}
+        <g id="tick">
+          {" "}
+          <polyline
+            fill="none"
+            points="3.7 14.3 9.6 19 20.3 5"
+            stroke="#000000"
+            stroke-linecap="round"
+            stroke-linejoin="round"
+            stroke-width="2"
+          ></polyline>{" "}
+        </g>{" "}
+      </g>{" "}
+    </g>
+  </svg>
+);
